dbd22a3a248e780de630fddf5a81dcb9c0ae971d
Alan Butler <rabutler@usbr.gov>
2015-11-20 10:58:31 -0600

    Initial load of data and scripts

---
+++ plotCosts.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(dplyr)
+
+zz <- read.csv('Costs.csv')
+
+# set any NA's to 0
+zz$Cost[is.na(zz$Cost)] <- 0
+
+# time series of costs
+dayTot <- zz %>% group_by(Date) %>% summarise(Tot = sum(Cost))
+
+g1 <- ggplot(dayTot, aes(Date, Tot,group = 'identity')) + geom_line() +
+  geom_hline(yintercept = mean(dayTot$Tot)) + 
+  annotate('text', label = paste('Avg Daily Cost =',round(mean(dayTot$Tot)),2), 
+           y = mean(dayTot$Tot) + diff(range(dayTot$Tot))*.05,
+           x = mean(ggplot_build(g1)$panel$ranges[[1]]$x.range))
+
+g1
+
+# costs by category
+# make Breakfast, Lunch, Dinner, Snack all = Food
+zz$Category <- as.character(zz$Category)
+catCost <- zz %>% 
+  dplyr::mutate(Category = replace(Category, Category %in% c('Breakfast', 'Lunch', 'Dinner','Snack'),'Food')) %>%
+  dplyr::group_by(Category) %>% dplyr::summarize(Tot = sum(Cost))
+zz$Category <- as.factor(zz$Category)
+
+g2 <- ggplot(catCost, aes(x = Category, y = Tot, fill = Category)) + geom_bar(stat = 'identity')
+g2
